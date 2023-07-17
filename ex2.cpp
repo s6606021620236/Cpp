6feb508dd1fcfc3685460855d990793ad22964a4
Suwat Chitcharoen <139624900+s6606021620236@users.noreply.github.com>
2023-07-17 09:58:28 +0700

    Add files via upload

---
+++ ex2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main()
+{
+string Name ;
+int Year ; 
+cout << "Enter Name :" ; 
+cin  >> Name ;
+cout << "Enter birth :" ;
+cin  >> Year ;
+cout << "Your Age = :" << 2566-Year << "years old"<<endl ;
+
+
+
+return (0);
+}
