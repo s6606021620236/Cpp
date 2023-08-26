06e494ba4808e42cac5f72762e50a9a1c26479d4
Suwat Chitcharoen <139624900+s6606021620236@users.noreply.github.com>
2023-08-26 15:20:16 +0700

    CountStr.cpp

--- CountStr.cpp
+++ CountStr.cpp
@@ -7,26 +7,22 @@ int main()
     int count1 = 0, count2 = 0, count3 = 0, count4 = 0;
     string str;
     cout << "Enter message: ";
-    getline(cin, str);  //  tum ma da mai nub space
+    getline(cin, str);  //  cin >> str tum ma da mai nub space
     
     for (int n = 0; n < str.length(); n++) {
         if (str[n] >= 'A' && str[n] <= 'Z')
             count1++;
-    }
-    
-    for (int n = 0; n < str.length(); n++) {
+            
         if (str[n] >= 'a' && str[n] <= 'z')
             count2++;
-    }
-
-    for (int n = 0; n < str.length(); n++) {
-        if (str[n] != 'a' && str[n] != 'z' && str[n] >= 'A' && str[n] <= 'Z' && str[n] != ' ')
-            count4++;
-    }
-    
-    for (int n = 0; n < str.length(); n++) {
+        
         if (str[n] == ' ')
             count3++;
+            
+        if (str[n] != 'a' && str[n] != 'z' && str[n] >= 'A' && str[n] <= 'Z' && str[n] != ' ')
+            count4++;
+
+
     }
     cout << "Your message has " << count1 << " uppers character."  << endl;
     cout << "Your message has " << count2 << " lowers character."  << endl;
