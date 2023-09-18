a57e79fbb58fa856d4e9a77b96c64bc1a7b8fdc4
Suwat Chitcharoen <139624900+s6606021620236@users.noreply.github.com>
2023-09-18 11:59:01 +0700

    array.cpp

--- array.cpp
+++ array.cpp
@@ -2,35 +2,43 @@
 #include <string>
 #include <iomanip>
 using namespace std;
+string check_result(int x);
 int main(){
-	int score[4] ,avg , total= 0  ,max = 0  , min=9999999999999999999 ; 	 
+	int score[4]    , total= 0  ,max = 0  ,min  ; 	 
 	string name[4] ;
-
+ 
 	for (int i = 0; i < 4; i++){ // i <= 3
 		cout <<"Enter Name [" << i <<"] : "  ;
 		cin >> name[i];
 		cout <<"Enter Score[" << i <<"] : "  ;
-		cin >>   score[i];
+		cin >> score[i];
 		total += score[i];
 
-	    if (score[i] <  min) 
-			min = score[i]; 
 		if (score[i] >  max) 
-			max = score[i];
+			max = score[i];	
+	}
+	min = score[0];
+	cout <<"---------------------------------------\n";
+	cout <<"Name           Score\t\tResult\n";
+	cout <<"---------------------------------------\n";
 
-		
-	}
-	cout <<"----------------------\n";
-	cout <<"Name           Score\n";
-	cout <<"----------------------\n";
 	for (int i = 0; i < 4; i++){  
-		cout << name[i] <<"\t\t"<< score[i] << endl ;
+		cout << name[i] <<"\t\t"<< score[i] << "\t\t"<< check_result(score[i]) << endl;
 	}
-	cout <<"----------------------\n";
+	cout <<"---------------------------------------\n";
 	cout <<"Max score     = " << max << endl;
 	cout <<"Min score     = " << min << endl;
 	cout <<"Total Score   = " << total << endl;
 	cout <<"Average Score = " <<(float)total/4 << endl;
- 
+ system ("pause");
 	return 0;
+}
+
+string check_result(int x){
+
+	if (x >= 50)
+	 return("pass");
+	else 
+	 return("failed");
+	
 }
