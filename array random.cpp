20d7aac2c07b43ad92822d0df99eeb6283386d9d
Suwat Chitcharoen <139624900+s6606021620236@users.noreply.github.com>
2023-09-24 22:14:44 +0700

    Add files via upload

---
+++ array random.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <iomanip>
+using namespace std;
+
+int main()
+{
+    int random[10] ;
+    srand(time(NULL));
+    cout << "Element  Value  Histogram" << endl;
+    cout << endl;
+    for (int i = 0; i < 10; i++)
+    {
+        random[i] = 1 + rand() % 30;
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout <<"    " << i << "    " << random[i] << "\t";
+        for (int j = 0; j < random[i]; j++)
+        {
+            cout << '*';
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
