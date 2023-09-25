5e6f7fb8ed02b1081a039e80ed6d301a6f092a4e
Suwat Chitcharoen <139624900+s6606021620236@users.noreply.github.com>
2023-09-25 12:10:07 +0700

    Add files via upload

--- ass.cpp
+++ ass.cpp
@@ -2,11 +2,12 @@
 #include <string>
 using namespace std;
 void input(int &nums,int quizz[10],int mid[10] ,int final[10]);
-void Viewall(int &nums,int quizz[10],int mid[10] ,int final[10]);
+void Viewall(int &nums,int quizz[10],int mid[10] ,int final[10],int Sum[10]);
 void viewID(int &nums,int quizz[10],int mid[10] ,int final[10]);
+void selectionSort(int nums,int Sum[10]);
 int main(){
 	int nums = 0;
-	int  quizz[10], mid[10], final[10], menu;
+	int  quizz[10], mid[10], final[10],Sum [10], menu;
 do {	
 	cout <<"===========================\n";
     cout <<"         MENU			   \n";
@@ -14,7 +15,8 @@ do {
     cout <<" 1.Input student records\n";
     cout <<" 2.View all student records\n";
     cout <<" 3.Show a student by ID\n";
-    cout <<" 4.Exit\n";
+	cout <<" 4.Sort sum Score\n";
+    cout <<" 5.Exit\n";
 
 	cout <<"Select menu :" ;
 	cin >> menu ;
@@ -23,15 +25,18 @@ do {
 		input(nums,quizz, mid, final);
 	}
 	else if(menu == 2) {
-		Viewall(nums,quizz, mid, final);
+		Viewall(nums,quizz, mid, final,Sum );
 
 	}
 	else if(menu == 3){
 		viewID(nums,quizz, mid, final);
 	}
-	else if(menu == 4)
+	else if(menu == 4){
+		selectionSort(nums,Sum);
+	}
+	else if(menu == 5)
 		cout <<"Exit..." <<endl;
-}while (menu != 4);
+}while (menu != 5);
 
 
 
@@ -54,14 +59,22 @@ void input(int &nums,int quizz[10],int mid[10] ,int final[10]){
 	}
 }
 
-void Viewall(int &nums,int quizz[10],int mid[10] ,int final[10]){
-
+void Viewall(int &nums,int quizz[10],int mid[10] ,int final[10],int Sum [10]){
+	float tq= 0.0, tm =0.0 ,tf = 0.0   ;
 	cout <<"--------------------------------------- "<< endl; 
-	cout <<"StdID   Quiz    Midterm  Final\n";
+	cout <<"StdID   Quiz    Midterm  Final   Sum\n";
 	cout <<"--------------------------------------- "<< endl; 
-	for(int i= 0 ; i < nums ; i++){
-	cout << i+1 <<"\t" <<quizz[i] <<"\t" << mid[i] <<"\t" << final[i]<< endl;
+	for(int i= 0 ; i < nums ; i++){ 
+	tq += quizz[i];
+	tm += mid[i];
+	tf += final[i];
+	Sum[i] = quizz[i]+ mid[i]+ final[i];
+	cout << i+1 <<"\t" <<quizz[i] <<"\t" << mid[i] <<"\t" << final[i] <<"\t" << Sum[i] << endl;
+	;
+	
 	}
+	cout <<"--------------------------------------- "<< endl;
+	cout <<"Total" <<"\t"<< tq << "\t"  << tm << "\t"<< tf << endl; 
 	cout <<"--------------------------------------- "<< endl; 
 } 
 
@@ -85,4 +98,28 @@ void viewID(int &nums,int quizz[10],int mid[10] ,int final[10]){
 			break;
 		}
 	}
+}
+
+
+void selectionSort(int nums,int Sum[10]) {
+
+	cout <<"------------------------"<< endl;
+	cout <<"No \tSum_Score"<< endl;
+	cout <<"------------------------"<< endl;
+for (int i = 0; i < nums -1; i++) {
+	int minIndex = i;
+	for (int j = i + 1; j < nums; j++) {
+		if (Sum[j] < Sum[minIndex]) {
+			minIndex = j;
+		}
+	}
+	int temp = Sum[i];
+	Sum[i] = Sum[minIndex];
+	Sum[minIndex] = temp;
+
+	}
+	for(int i =0; i <nums ; i++){
+		cout <<i+1 <<"\t   "<< Sum[i] << endl;
+	}
+	cout <<"------------------------"<< endl;
 }
