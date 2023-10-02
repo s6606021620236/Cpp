bdd8bf0f814434337c48a0c2243b9f87508335a1
Suwat Chitcharoen <139624900+s6606021620236@users.noreply.github.com>
2023-10-02 11:20:13 +0700

    Add files via upload

---
+++ arr2.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+#include<string>
+using namespace std;
+int main(){ 
+	int num[3][3] ;
+	string name[3];
+	for(int i=0; i<3;i++){
+		cout <<"Student "<< i+1 <<endl;
+		cout <<"Name [" << i+1 <<"] : ";
+		cin>>name[i];
+	
+		for (int j=0; j<3; j++){
+			cout <<"Enter Score [" << i+1 <<"]["<< j+1 <<"] :";
+			cin >> num [i][j];
+		}
+		
+	}
+	cout <<"Show Score" <<endl;	
+	cout <<"---------------------------------\n" ;
+	cout <<"Student\tMath\tScience\tEnglish\n";
+	cout <<"---------------------------------\n"  ;
+	for(int r = 0; r<3; r++ ){
+		cout << r+1 <<"   " << name[r] <<"\t" ;
+		for(int c = 0 ;c<3; c++){
+			cout << num[r][c] <<"\t";
+		}
+		cout << endl;
+	}
+
+
+	system("pause");
+	return 0;
+}
+
